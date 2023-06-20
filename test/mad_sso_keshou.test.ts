c3b4a522ba1f589eb335250f5677a65a7be5642f
bun913 <imanau03@gmail.com>
2023-06-20 19:33:44 +0900

    Initial commit

---
+++ test/mad_sso_keshou.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MadSsoKeshou from '../lib/mad_sso_keshou-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mad_sso_keshou-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MadSsoKeshou.MadSsoKeshouStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
